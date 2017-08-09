65c2bb56ddf0678fef5de4a9487b2b1bbf03b954
admin <admin@163.com>
2017-08-09 17:50:26 +0800

    gx

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <my-header></my-header>
+    <div>tab</div>
+    <div>content</div>
+  </div>
+</template>
+
+<script>
+  import Header from './components/Header.vue'
+  export default {
+    name: 'app',
+    components:{
+        MyHeader:Header
+    }
+  }
+</script>
+
+<style>
+</style>
